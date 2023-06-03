d469220531eb6f91972cd242b7353d20cae0e08b
Vincenzo Silva Fadda <vincenzofadda@gmail.com>
2023-06-03 16:37:28 -0300

    counter

--- scripts/script.js
+++ scripts/script.js
@@ -29,3 +29,46 @@ function VerifyActive(item, content, content_actived) {
     content.style.height = content.scrollHeight + 10 + "px";
   }
 }
+
+let valueDisplays = document.querySelectorAll(".number-counter");
+let interval = 5000;
+
+// Função para verificar se o elemento está visível no viewport
+function isElementVisible(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Função para animar o contador
+function animateCounter(element) {
+  let startValue = 0;
+  let endValue = parseInt(element.getAttribute("finalValue"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function() {
+    startValue += 5;
+    element.textContent = startValue + "+";
+    if (startValue >= endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+}
+
+// Observador de interseção para ativar o contador quando o elemento estiver visível
+let observer = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      animateCounter(entry.target);
+      observer.unobserve(entry.target);
+    }
+  });
+});
+
+// Observar cada elemento com a classe "number-counter"
+valueDisplays.forEach(valueDisplay => {
+  observer.observe(valueDisplay);
+});
